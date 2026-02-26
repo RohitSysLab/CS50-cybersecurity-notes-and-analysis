b64e5c32bd575072dc215b8d84742fb22b1305c8
RohitSysLab <studienRohit@gmail.com>
2026-02-26 22:43:13 +0530

    Update crack.py

--- securing-accounts/code/crack.py
+++ securing-accounts/code/crack.py
@@ -1,1 +1,11 @@
 
+import string
+
+letters = string.ascii_letters
+
+for a in letters:
+    for b in letters:
+        for c in letters:
+            for d in letters:
+                guess = a + b + c + d
+                print(guess)
